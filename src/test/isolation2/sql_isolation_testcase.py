e6a5ac8e6aaeb04e0543f0ed453cd0c316e2ba43
Tyler Ramer <tyaramer@gmail.com>
2018-11-06 12:48:53 -0500

    WIP: sql_isolation2:
    Null notice receiver to silently drop notice messages

--- src/test/isolation2/sql_isolation_testcase.py
+++ src/test/isolation2/sql_isolation_testcase.py
@@ -138,6 +138,14 @@ class SQLIsolationExecutor(object):
             else:
                 self.con = self.connectdb(self.dbname)
 
+
+        def null_notice_receiver(notice):
+            '''
+                Tests ignore notice messages when analyzing results
+                This nul function silently drop notices from the pg.connection
+            '''
+            return
+
         def connectdb(self, given_dbname, given_host = None, given_port = None, given_opt = None):
             con = None
             retry = 1000
@@ -161,6 +169,7 @@ class SQLIsolationExecutor(object):
                         time.sleep(0.1)
                     else:
                         raise
+            con.set_notice_receiver(self.null_notice_receiver)
             return con
 
         def get_utility_mode_port(self, name):
